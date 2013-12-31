af0a1442ff27008600075e95f149985ff522a6a8
Michael Barrington Stone <hi@michaelbstone.net>
2013-12-31 00:56:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#Canada Time Tracker
+
+This application rose out of a need to find a much better solution to track the number of days my company's employees spent
+in Canada. The current method is *cumbersome* as there is no automatic method to keep track of the number of days one spends 
+in Canada. The best solution is to update a calendar on an Excel document. 
+
+This application will hopefully solve that problem.
+
+by [Michael Barrington Stone](http://www.michaelbstone.net)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CanadaTimeTracker::Application.config.secret_key_base = 'f1cf8181f6e5db71907b49d46c8423225d99febd0ca5aa975951ab30a15849b83bbda737cc7375c2ec361e95855809816a9916fe69d82da751fea02a7df91799'
