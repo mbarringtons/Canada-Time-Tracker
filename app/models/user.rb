f8a9ca45ec239a5319c41482f058842ccd9a9e4e
Michael Barrington Stone <hi@michaelbstone.net>
2014-02-19 20:40:57 -0500

    Added Account Settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :user_country, presence: true
   
+  
+  # \A matches beginning of string, where [^@\s] means @ should ONLY precede capgemini, as it's ensuring that it's not matched for this.
   validates_format_of :email, :with => /\A([^@\s]+)@(capgemini\.com)\z/
 end
